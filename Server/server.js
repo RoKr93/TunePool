7a06a2acbcb8787170c38fc5c4bdb98605058d7e
Roshan Krishnan <r.krishnan1@umiami.edu>
2016-02-06 17:37:37 -0500

    created extremely rudimentary web server framework

--- Server/server.js
+++ Server/server.js
@@ -1,31 +1,21 @@
-/*var http = require("http");
-
-var server = http.createServer(function(request, response){
-    var queryData = url.parse(request.url, true).query;
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    
-    
-});
-
-server.listen(8888);*/
-
 // import express
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-// import the body parser
-var bodyParser = require('body-parser');
+// configure the body parser
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // respond to post requests
-app.post('/operation', function(req, res){
+app.post('/', function(req, res){
+    // get all the information
     var username = req.body.username;
-    console.log("result: " + username);
+    var user_id = req.body.id;
     res.send("Got it!");
 });
 
 // listen
 app.listen(8888, function () {
-  console.log('Example app listening on port 8888!');
+  console.log('Listening on port 8888!');
 });
